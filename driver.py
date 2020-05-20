89ed63ca8c8aa42de32ca8bd85602436b4d9bf7a
Omer Eroglu <omereroglu516@gmail.com>
2020-05-20 11:02:02 +0300

    file_operations

--- driver.py
+++ driver.py
@@ -1,3 +1,74 @@
+# -*- coding: utf-8 -*-
+# importing the csv module
+import csv
+#importing regex
+import re
+#importing random module
+import random
+
+def randomDataGenerator():
+    #creating random sensorNum
+    sensorNum=random.randint(1, 8)
+    #random result of attempt
+    isSuccess=random.randint(0,1)
+
+    return(sensorNum,isSuccess)
+
+def trainingProcess(playerID,trainingID):
+    #accuracy training constant=30 times sensors will light
+    ACCURACY_CONSTANT=30
+    # name of csv file
+    filename = "training_process.csv"
+    # field names
+    fields = ['playerID', 'trainingID', 'sensorNum', 'isSuccess']
+    accuracyTraining=re.compile("111*")
+    speedTraining=re.compile("000*")
+
+    #it means it is a accuracy training
+    if(re.match(accuracyTraining,trainingID)):
+        for i in range(ACCURACY_CONSTANT):
+            print("accuracy ",i)
+            with open(filename, 'a+') as csvfile:
+                # creating a csv dict writer object
+                writer = csv.DictWriter(csvfile, fieldnames=fields)
+
+
+                randomData=randomDataGenerator()
+                #light sensor
+                sensorLight=randomData[0]
+                #getting random result of random attempt
+                isSuccess=randomData[1]
+                mydict=[{'playerID':playerID,'trainingID':trainingID,'sensorNum':sensorLight,'isSuccess':isSuccess}]
+                writer.writerows(mydict)
+                csvfile.close()
+    #speed training
+    else:
+        randomSpeed=random.randint(30,45)
+        for i in range(randomSpeed):
+            print("speed ",i)
+            with open(filename, 'a+') as csvfile:
+                # creating a csv dict writer object
+                writer = csv.DictWriter(csvfile, fieldnames=fields)
+
+
+                randomData = randomDataGenerator()
+                # light sensor
+                sensorLight = randomData[0]
+                # getting random result of random attempt
+                isSuccess = randomData[1]
+                mydict = [
+                    {'playerID': playerID, 'trainingID': trainingID, 'sensorNum': sensorLight, 'isSuccess': isSuccess}]
+                writer.writerows(mydict)
+                csvfile.close()
+#creating random trainingID
+def randomTrainingID(type):
+    if type == 0:
+        #we need database connection to create actuall trainingID. This is just simulation
+        return '1119'
+
+    elif trainingType == 1:
+        # we need database connection to create actuall trainingID. This is just simulation
+        return '00011'
 """
 Izmir University of Economics Faculty of Engineering
 Project Name: Performance Analysis Technology
@@ -11,13 +82,15 @@ Project Group: Ömer EROĞLU - Fatih KOCA - Oğuz Timur TAŞDELEN
 print("\n***WELCOME TO PERFORMANCE ANALYSIS TECHNOLOGY SYSTEM*** \n")
 print("Performance Analysis Technology is used to measure performance of players in wide range sports branch and "
       "to serve this measurements to coaches.")
-
-enterKey = input("Press Enter to Start!")
-# Checks if input is 'Enter' key or not.
+enterKey="a"
 while enterKey != "":
-    continue
+    enterKey = str(input("Press Enter to Start!"))
+# Checks if input is 'Enter' key or not.
+
+
 
 print("\n\n\n")
+print("Process begins")
 
 # Section: 'Prepare the Platform'
 coachID = input('Enter Coach ID: ')  # Check if coach exists!
@@ -26,35 +99,32 @@ playerID = input('Enter Player ID: ')  # Check if player exists!
 print("\n0: Accuracy Measurement Training | 1: Speed Measurement Training")
 trainingType = int(input('Select Training Type (Press 0 or 1): '))
 # Actual implementation will not be terminal application. Therefore no need to check if input is valid or not.
+trainingID = randomTrainingID(trainingType)
 
 
-enterKey = input("\n\n\nTraining Platform is Ready! Press Enter to Start!")
-# Checks if input is 'Enter' key or not.
+
+enterKey="a"
 while enterKey != "":
-    continue
+    enterKey = str(input("Press Enter to Start!"))
+# Checks if input is 'Enter' key or not.
 
 print("\n\n\n")
 
-
-# Section: 'Define Training Methods'
-def accuracyTraining():
-    """
-    Accuracy Training Method will be implemented here!
-
-    """
-    print("Accuracy Training Section")
+# after pressed enter key training process can start
+trainingProcess(playerID,trainingID)
+print("\n\n\n")
+print("Process finished")
 
 
-def speedTraining():
-    """
-    Speed Training Method will be implemented here!
-
-    """
-    print("Speed Training Section")
+#process is completed for one step for now
 
 
-if trainingType == 0:
-    accuracyTraining()
 
-elif trainingType == 1:
-    speedTraining()
+
+
+
+
+
+
+
+
